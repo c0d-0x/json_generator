b5678dce2b01dcaaadb03782f8d9250d123b5c5a
c0d-0x <c0d_0x007@proton.me>
2025-01-02 12:42:08 +0100

    improvement: reduced validity  check. features: backup after exceeding file 10MB and rename backup as current date

--- src/json_gen.h
+++ src/json_gen.h
@@ -2,6 +2,7 @@
 #define JSON_GEN_H
 
 #include <errno.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,14 +15,15 @@
 #define OFFSET 3
 #endif  // _WIN32 or __linux__
 
+#define CUSTOM_ERR (-1)
 #define JSON_FILE "sample_gen.json"
 
+/*10 MB max */
+#define FILE_SIZE_MAX 10485760
+/*{}\r\n: json obj file or []\r\n: json array file*/
+#define BEGIN_SYMBOL "[]\r\n"
 enum FILE_STATE { EMPTY_FILE = 0, NOT_FOUND, VALID_JSON, INVALID_JSON };
 
-/**
- * This supposed to be custom generated, to suite your json object.
- * Use this structure to construct your json_constructor function.
- * */
 typedef struct {
   char *date;
   char *file;
@@ -32,8 +34,15 @@ typedef struct {
   char *e_p_state;
 } json_obj_t;
 
-void append_to_file(FILE *json_fp, json_obj_t json_obj,
-                    void (*json_constructor)(FILE *, json_obj_t));
+void json_constructor(FILE *json_fp, json_obj_t *json_obj);
+void write_json_obj(FILE *json_fp, json_obj_t *json_obj,
+                    void (*json_constructor)(FILE *, json_obj_t *));
+
 size_t validate_json(char *json_file);
+void backup_file(char *file_path);
+size_t get_file_size(char *file_path);
+FILE *create_new_log(const char *file_path, const char *begin_symbol);
+FILE *init_json_gen();
+void close_json_file(FILE *json_fp);
 
 #endif  // !JSON_GEN_H
